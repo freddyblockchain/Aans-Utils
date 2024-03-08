a1efcdc746dcf005752e2a13d3cd849e35508299
Frederik <frederikblockchain@gmail.com>
2024-03-08 16:00:49 +0100

    fix names

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { createaanTransaction } from './create-aan';
-export { getaanNames } from './get-names';
-export { getaanAccountAddress } from './get-names';
+export { createAanTransaction } from './create-aan';
+export { getAanNames } from './get-names';
+export { getAanAccountAddress } from './get-names';
